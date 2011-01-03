8dd591ca08833e8ed3a6105c87ce67975d82c292
dandromereschi <dandromereschi@0b5bfab6-fc16-11dd-ac65-f74f249cfaac>
2011-01-03 13:28:51 +0000

    A fix for issue #27.

--- achartengine/src/org/achartengine/chart/XYChart.java
+++ achartengine/src/org/achartengine/chart/XYChart.java
@@ -257,7 +257,7 @@ public abstract class XYChart extends AbstractChart {
           drawText(canvas, mRenderer.getXTitle(), x + width / 2, bottom + 24, paint, 0);
           drawText(canvas, mRenderer.getYTitle(), x + 10, y + height / 2, paint, -90);
           paint.setTextSize(mRenderer.getChartTitleTextSize());
-          drawText(canvas, mRenderer.getChartTitle(), x + width / 2, top + 10, paint, 0);
+          drawText(canvas, mRenderer.getChartTitle(), x + width / 2, y + mRenderer.getChartTitleTextSize(), paint, 0);
         } else if (or == Orientation.VERTICAL) {
           drawText(canvas, mRenderer.getXTitle(), x + width / 2, y + height - 10, paint, -90);
           drawText(canvas, mRenderer.getYTitle(), right + 20, y + height / 2, paint, 0);
