a524e92c2707cbf19294a2bfecd4689d44790e54
dandromereschi <dandromereschi@0b5bfab6-fc16-11dd-ac65-f74f249cfaac>
2012-01-04 21:15:53 +0000

    Fix for issue #157.

--- achartengine/src/org/achartengine/renderer/DialRenderer.java
+++ achartengine/src/org/achartengine/renderer/DialRenderer.java
@@ -25,10 +25,6 @@ import org.achartengine.util.MathHelper;
  * Dial chart renderer.
  */
 public class DialRenderer extends DefaultRenderer {
-  /** The chart title. */
-  private String mChartTitle = "";
-  /** The chart title text size. */
-  private float mChartTitleTextSize = 15;
   /** The start angle in the dial range. */
   private double mAngleMin = 330;
   /** The end angle in the dial range. */
@@ -46,42 +42,6 @@ public class DialRenderer extends DefaultRenderer {
 
   public enum Type {
     NEEDLE, ARROW;
-  }
-
-  /**
-   * Returns the chart title.
-   * 
-   * @return the chart title
-   */
-  public String getChartTitle() {
-    return mChartTitle;
-  }
-
-  /**
-   * Sets the chart title.
-   * 
-   * @param title the chart title
-   */
-  public void setChartTitle(String title) {
-    mChartTitle = title;
-  }
-
-  /**
-   * Returns the chart title text size.
-   * 
-   * @return the chart title text size
-   */
-  public float getChartTitleTextSize() {
-    return mChartTitleTextSize;
-  }
-
-  /**
-   * Sets the chart title text size.
-   * 
-   * @param textSize the chart title text size
-   */
-  public void setChartTitleTextSize(float textSize) {
-    mChartTitleTextSize = textSize;
   }
 
   /**
