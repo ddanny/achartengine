9b196bc527f7cbc06b9ffb9562d1a3f99e9bdb5d
dandromereschi@gmail.com <dandromereschi@gmail.com@0b5bfab6-fc16-11dd-ac65-f74f249cfaac>
2013-01-30 18:47:41 +0000

    Added some comments to the example code.

--- trunk/achartengine/demo/org/achartengine/chartdemo/demo/chart/PieChartBuilder.java
+++ trunk/achartengine/demo/org/achartengine/chartdemo/demo/chart/PieChartBuilder.java
@@ -34,20 +34,17 @@ import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class PieChartBuilder extends Activity {
-  public static final String TYPE = "type";
-
+  /** Colors to be used for the pie slices. */
   private static int[] COLORS = new int[] { Color.GREEN, Color.BLUE, Color.MAGENTA, Color.CYAN };
-
+  /** The main series that will include all the data. */
   private CategorySeries mSeries = new CategorySeries("");
-
+  /** The main renderer for the main dataset. */
   private DefaultRenderer mRenderer = new DefaultRenderer();
-
-  private String mDateFormat;
-
+  /** Button for adding entered data to the current series. */
   private Button mAdd;
-
-  private EditText mX;
-
+  /** Edit text field for entering the slice value. */
+  private EditText mValue;
+  /** The chart view that displays the data. */
   private GraphicalView mChartView;
 
   @Override
@@ -55,7 +52,6 @@ public class PieChartBuilder extends Activity {
     super.onRestoreInstanceState(savedState);
     mSeries = (CategorySeries) savedState.getSerializable("current_series");
     mRenderer = (DefaultRenderer) savedState.getSerializable("current_renderer");
-    mDateFormat = savedState.getString("date_format");
   }
 
   @Override
@@ -63,46 +59,37 @@ public class PieChartBuilder extends Activity {
     super.onSaveInstanceState(outState);
     outState.putSerializable("current_series", mSeries);
     outState.putSerializable("current_renderer", mRenderer);
-    outState.putString("date_format", mDateFormat);
   }
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.xy_chart);
-    mX = (EditText) findViewById(R.id.xValue);
-    mRenderer.setApplyBackgroundColor(true);
-    mRenderer.setBackgroundColor(Color.argb(100, 50, 50, 50));
-    mRenderer.setChartTitleTextSize(20);
-    mRenderer.setLabelsTextSize(15);
-    mRenderer.setLegendTextSize(15);
-    mRenderer.setMargins(new int[] { 20, 30, 15, 0 });
+    mValue = (EditText) findViewById(R.id.xValue);
     mRenderer.setZoomButtonsVisible(true);
-    mRenderer.setStartAngle(90);
+    mRenderer.setStartAngle(180);
+    mRenderer.setDisplayValues(true);
 
     mAdd = (Button) findViewById(R.id.add);
     mAdd.setEnabled(true);
-    mX.setEnabled(true);
+    mValue.setEnabled(true);
 
     mAdd.setOnClickListener(new View.OnClickListener() {
       public void onClick(View v) {
-        double x = 0;
+        double value = 0;
         try {
-          x = Double.parseDouble(mX.getText().toString());
+          value = Double.parseDouble(mValue.getText().toString());
         } catch (NumberFormatException e) {
-          // TODO
-          mX.requestFocus();
+          mValue.requestFocus();
           return;
         }
-        mSeries.add("Series " + (mSeries.getItemCount() + 1), x);
+        mValue.setText("");
+        mValue.requestFocus();
+        mSeries.add("Series " + (mSeries.getItemCount() + 1), value);
         SimpleSeriesRenderer renderer = new SimpleSeriesRenderer();
         renderer.setColor(COLORS[(mSeries.getItemCount() - 1) % COLORS.length]);
         mRenderer.addSeriesRenderer(renderer);
-        mX.setText("");
-        mX.requestFocus();
-        if (mChartView != null) {
-          mChartView.repaint();
-        }
+        mChartView.repaint();
       }
     });
   }
@@ -114,40 +101,22 @@ public class PieChartBuilder extends Activity {
       LinearLayout layout = (LinearLayout) findViewById(R.id.chart);
       mChartView = ChartFactory.getPieChartView(this, mSeries, mRenderer);
       mRenderer.setClickEnabled(true);
-      mRenderer.setSelectableBuffer(10);
       mChartView.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
           SeriesSelection seriesSelection = mChartView.getCurrentSeriesAndPoint();
           if (seriesSelection == null) {
-            Toast
-                .makeText(PieChartBuilder.this, "No chart element was clicked", Toast.LENGTH_SHORT)
+            Toast.makeText(PieChartBuilder.this, "No chart element selected", Toast.LENGTH_SHORT)
                 .show();
           } else {
+            for (int i = 0; i < mSeries.getItemCount(); i++) {
+              mRenderer.getSeriesRendererAt(i).setHighlighted(i == seriesSelection.getPointIndex());
+            }
+            mChartView.repaint();
             Toast.makeText(
                 PieChartBuilder.this,
-                "Chart element data point index " + seriesSelection.getPointIndex()
-                    + " was clicked" + " point value=" + seriesSelection.getValue(),
-                Toast.LENGTH_SHORT).show();
-          }
-        }
-      });
-      mChartView.setOnLongClickListener(new View.OnLongClickListener() {
-        @Override
-        public boolean onLongClick(View v) {
-          SeriesSelection seriesSelection = mChartView.getCurrentSeriesAndPoint();
-          if (seriesSelection == null) {
-            Toast.makeText(PieChartBuilder.this, "No chart element was long pressed",
-                Toast.LENGTH_SHORT).show();
-            return false; // no chart element was long pressed, so let something
-            // else handle the event
-          } else {
-            Toast.makeText(
-                PieChartBuilder.this,
-                "Chart element data point index " + seriesSelection.getPointIndex()
-                    + " was long pressed", Toast.LENGTH_SHORT).show();
-            return true; // the element was long pressed - the event has been
-            // handled
+                "Chart data point index " + seriesSelection.getPointIndex() + " selected"
+                    + " point value=" + seriesSelection.getValue(), Toast.LENGTH_SHORT).show();
           }
         }
       });
